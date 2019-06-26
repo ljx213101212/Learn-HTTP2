9c135215c7d8050eacb52a18f71b09c3d2954646
Li Jixiang <jixiang.li@razer.com>
2019-06-26 18:01:52 +0800

    Init project.

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const http2 = require('http2');
+const fs = require('fs');
+
+const server = http2.createSecureServer({
+  key: fs.readFileSync('localhost-privkey.pem'),
+  cert: fs.readFileSync('localhost-cert.pem')
+});
+server.on('error', (err) => console.error(err));
+
+server.on('stream', (stream, headers) => {
+  // stream is a Duplex
+  stream.respond({
+    'content-type': 'text/html',
+    ':status': 200
+  });
+  stream.end('<h1>Hello World</h1>');
+});
+
+server.on('session', (session) => {
+    // Set altsvc for origin https://example.org:80
+    session.altsvc('h2=":8000"', 'https://localhost:8443');
+  });
+  
+
+server.listen(8443)
